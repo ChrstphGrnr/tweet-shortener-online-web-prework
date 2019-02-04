ccac293ccbe6542b4b453acb9e6eea7f2f1a5073
Christoph Gruener <christophgruener@me.com>
2019-02-04 23:00:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def dictionary
  
   dictionary = {  :"hello" => "hi", :"to" => "2", :"two" => "2", :"too" => "2", :"for" => "4", :"four" => "4", :"be" => "b", :"you" => "u",  :"at" => "@", :"and" => "&"}
@@ -6,9 +7,10 @@ end
 
 def word_substituter(tweets)
   tweets_ary = tweets.split(" ")
+  binding.pry
     tweets_ary.each.with_index do |word, index|
       if dictionary.has_key?(word)
-        tweets_ary[index] = dictionary[word]
+        tweets_ary[index] = dictionary:word]
       end
     end
   tweets_ary.join(" ")
